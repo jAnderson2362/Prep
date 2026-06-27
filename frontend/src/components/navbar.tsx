ee07573c310d99044bb64ffb4738de24fdc73f4f
JT <jamestasker2004@gmail.com>
2026-06-27 12:05:39 +1200

    added active link highlighting, added sign in buttons, changed logo placeholder, changed colours

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -16,26 +16,132 @@ const Navbar = () => {
 
     return (
         <nav
-            className="sticky top-0 z-50 flex w-full items-center justify-between bg-[#0048FF]/50 px-5 py-4 shadow-sm lg:px-8 xl:px-[8%]"
+            className="sticky top-0 z-50 flex w-full items-center justify-between bg-[#81a3f8] px-5 py-4 shadow-sm lg:px-8 xl:px-[8%]"
             // ${isScroll ? "bg-white/50 backdrop-blur-lg shadow-sm" : ""}`}
         >
-            {/* Logo */}
+            {/* Logo placeholder*/}
             <Link to="/" className="font-bold text-lg w-28">
-                LOGO
+                PREP
             </Link>
 
             {/* Nav */}
             <ul className="hidden md:flex items-center gap-8 rounded-full px-12 py-3">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/products">Products</Link></li>
-                <li><Link to="/">Features</Link></li>
-                <li><Link to="/">Community</Link></li>
-                <li><Link to="/">Pricing</Link></li>
-                <li><Link to="/">Contact</Link></li>
+                <li>
+                    <Link
+                        to="/"
+                        activeOptions={{ exact: true }}
+                        activeProps={{
+                            className: "border-b-2 border-blue-600 text-blue-600",
+                        }}
+                        inactiveProps={{
+                            className: "hover:text-blue-600",
+                        }}
+                    >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/about"
+                        activeOptions={{ exact: true }}
+                        activeProps={{
+                            className: "border-b-2 border-blue-600 text-blue-600",
+                        }}
+                        inactiveProps={{
+                            className: "hover:text-blue-600",
+                        }}
+                    >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/products"
+                        activeOptions={{ exact: true }}
+                        activeProps={{
+                            className: "border-b-2 border-blue-600 text-blue-600",
+                        }}
+                        inactiveProps={{
+                            className: "hover:text-blue-600",
+                        }}
+                    >
+                        Products
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/"
+                        activeOptions={{ exact: true }}
+                        activeProps={{
+                            className: "border-b-2 border-blue-600 text-blue-600",
+                        }}
+                        inactiveProps={{
+                            className: "hover:text-blue-600",
+                        }}
+                    >
+                        Features
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/"
+                        activeOptions={{ exact: true }}
+                        activeProps={{
+                            className: "border-b-2 border-blue-600 text-blue-600",
+                        }}
+                        inactiveProps={{
+                            className: "hover:text-blue-600",
+                        }}
+                    >
+                        Community
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/"
+                        activeOptions={{ exact: true }}
+                        activeProps={{
+                            className: "border-b-2 border-blue-600 text-blue-600",
+                        }}
+                        inactiveProps={{
+                            className: "hover:text-blue-600",
+                        }}
+                    >
+                        Pricing
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/"
+                        activeOptions={{ exact: true }}
+                        activeProps={{
+                            className: "border-b-2 border-blue-600 text-blue-600",
+                        }}
+                        inactiveProps={{
+                            className: "hover:text-blue-600",
+                        }}
+                    >
+                        Contact
+                    </Link>
+                </li>
             </ul>
 
-            {/* Mobile button */}
+            <div className="flex items-center gap-4 min-w-[220px] justify-end">
+                <Link
+                    to="/"
+                    className="text-sm bg-white text-black hover:text-blue border border-zinc-700 px-3 py-1.5 rounded"
+                >
+                    Sign in
+                </Link>
+                <Link
+                    to="/"
+                    className="text-sm bg-black text-white hover:text-blue border border-zinc-700 px-3 py-1.5 rounded"
+                >
+                    Register
+                </Link>
+            </div>
+
+            {/* Mobile borgir button */}
             <button
                 className="md:hidden"
                 onClick={() => setMenuOpen(true)}
@@ -45,7 +151,7 @@ const Navbar = () => {
 
             {/* Mobile Nav */}
             <div
-                className={`fixed top-0 right-0 h-screen w-64 bg-white transition-transform duration-300 md:hidden
+                className={`fixed top-0 right-0 h-screen w-64 bg-[#1cb1ecff] transition-transform duration-300 md:hidden
                 ${menuOpen ? "translate-x-0" : "translate-x-full"}`}
             >
                 <button
