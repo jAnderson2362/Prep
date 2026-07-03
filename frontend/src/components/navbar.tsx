a594569cb0f4b87b0a01dc43added2056ff1a2ab
JT <jamestasker2004@gmail.com>
2026-07-04 11:53:56 +1200

    changed mobile menu navbar bg to same colour

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -151,7 +151,7 @@ const Navbar = () => {
 
             {/* Mobile Nav */}
             <div
-                className={`fixed top-0 right-0 h-screen w-64 bg-[#1cb1ecff] transition-transform duration-300 md:hidden
+                className={`fixed top-0 right-0 h-screen w-64 bg-[#81a3f8] transition-transform duration-300 md:hidden
                 ${menuOpen ? "translate-x-0" : "translate-x-full"}`}
             >
                 <button
