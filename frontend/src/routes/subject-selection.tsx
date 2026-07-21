f5fd0cd4e8fec83e52fc91a2b56d1789e214e19c
JT <jamestasker2004@gmail.com>
2026-07-21 12:30:54 +1200

    added mode selection page and directed the button on subject selection to this page

--- frontend/src/routes/subject-selection.tsx
+++ frontend/src/routes/subject-selection.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate  } from '@tanstack/react-router'
+
 
 
 export const Route = createFileRoute('/subject-selection')({
@@ -6,7 +7,7 @@ export const Route = createFileRoute('/subject-selection')({
 })
 
 function SubjectSelection() {
-  
+  const navigate = useNavigate();
   return (
     <main className="min-h-screen bg-slate-50 p-6 flex justify-center">
       <div className="max-w-3xl rounded-2xl border border-slate-200 bg-white p-10 shadow-sm">
@@ -89,6 +90,7 @@ function SubjectSelection() {
        
         <div className="mt-8">
           <button
+            onClick={() => navigate({ to: "/mode-selection" })}
             className="w-full rounded-lg bg-blue-600 py-2 px-4 text-white hover:bg-blue-700"
             >
               Let's go!
