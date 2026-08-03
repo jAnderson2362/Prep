6d068426e6fafd9c891ff44b5a66ca1e1fea7bc1
Rafaela-Rossiya <tolosa.rafaela.rg@gmail.com>
2026-08-03 19:28:38 +1200

    Updated progress
    I just moved the schema documentation on the prep, apparently I created it outside the prep folder

--- backend/models/progress.py
+++ backend/models/progress.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 class Progress(BaseModel):
     user_id: str
     topic_id: int
+    score: int
     total_questions: int
     attempted_at: datetime
 class ProgressInDB(Progress):
